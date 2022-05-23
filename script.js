eaf20e1ae80649f248edf729cdfe9810f2439b55
John <john.houston0411@gmail.com>
2022-05-23 11:33:13 -0500

    change the grid

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.addEventListener("load", (event) => {
   const scrn = document.querySelector(".grid-screen")
   // let gridItem = document.createElement("div");
 
-  for(x = 0; x < 400; x++){
-      let gridItem = document.createElement("div");
+  for(x = 0; x < 1024; x++){
+    let gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
     scrn.appendChild(gridItem);
   }
